db76a507dc450eb5cfb6a90d80d7fd1859fd19d2
HeyStenson <h.n.stenson@gmail.com>
2015-10-07 16:07:16 -0700

    added an avatar for performers

--- app/controllers/performers_controller.rb
+++ app/controllers/performers_controller.rb
@@ -54,6 +54,6 @@ class PerformersController < ApplicationController
   private
 
   def performer_params
-    params.require(:performer).permit(:user_id, :performer_name, :performer_email, :city, :category_id, :subcategory, :hourly_rate, :description, :image1, :image2, :image3, :video1, :video2, :video3)
+    params.require(:performer).permit(:user_id, :performer_name, :performer_email, :city, :category_id, :subcategory, :hourly_rate, :description, :perfavatar, :image1, :image2, :image3, :video1, :video2, :video3)
   end
 end
--- app/models/performer.rb
+++ app/models/performer.rb
@@ -1,6 +1,10 @@
 class Performer < ActiveRecord::Base
 	belongs_to :user
 
+	has_attached_file :perfavatar, styles: { thumb: "100x100!" }, :default_style => :thumb, default_url: "/images/:style/missing.png"	
+	validates :perfavatar,
+		attachment_content_type: {content_type: /\Aimage\/.*\Z/ }
+
 	has_attached_file :image1, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image1, content_type: /\Aimage\/.*\Z/
 
