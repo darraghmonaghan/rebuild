81b3833e4119214926e6f2d77eb7af8c7a6870e5
Neil Rayos <Kneelhere@Neils-MacBook-Pro.local>
2015-10-08 12:52:55 -0700

    fixed performer profile link

--- app/controllers/performers_controller.rb
+++ app/controllers/performers_controller.rb
@@ -21,14 +21,44 @@ class PerformersController < ApplicationController
       end
   end
 
+  # def show
+  #   # need to connect user's performer_id and the performer id fields
+  #   @performer = Performer.find_by_id(params[:id]) #this line should be finding the performer by the performer id
+
+  #   category_id = @performer.category_id #should be finding the performer's category id
+  #   @category = Category.find_by_id(category_id) #should be querying the cat. database using category_id
+
+  #   @user = User.find_by_id(@performer.user_id) #should be finding the user id by the performer_id
+  #   @video1 = @performer.video1 #finding the first video for the performer
+  # end
+
+
   def show
-    # need to connect user's performer_id and the performer id fields
-    @performer = Performer.find_by_id(params[:id])
-    id = @performer.category_id
-    @category = Category.find_by_id(id)
-    @user = User.find_by_id(@performer.user_id)
-    @video1 = @performer.video1
+    puts "PARAMS" , params.inspect
+    @performer_id = params[:id]
+    # puts "PERF ID: ", @performer_id
+    @performer = Performer.find(@performer_id)
+    # puts "PERF OBJECT: ", @performer.inspect
+    # @user_id = @performer.user_id
+    # puts "USER ID: ", @user_id
+    # @user = User.find(@user_id)
+    # puts "USER OBJECT: ", @user.inspect
+
+    #@performers = Performer.where(user_id: user_found.user_id)
+    #@performer = @user_found.performers.collect(&:id)
+
+    # #@performer = Performer.find_by_id(@performer_id)
+
+    # @user = User.find_by_id(@performer.user_id) #should be finding the user id by the performer_id
+    # binding.pry
+    # @video1 = @performer.video1 #finding the first video for the performer
+
   end
+
+
+
+
+
 
   def edit
     @performer = Performer.find(params[:id])
