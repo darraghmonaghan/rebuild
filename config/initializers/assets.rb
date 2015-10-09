453961f5940d8c883a0bfeb7a5f378609cea42a7
Lisa Lehrman <lisa.lehrman@gmail.com>
2015-10-09 09:22:05 -0700

    final touches

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.precompile += %w( users.css )
 Rails.application.config.assets.precompile += %w( sessions.css )
 Rails.application.config.assets.precompile += %w( performers.css )
 
+
